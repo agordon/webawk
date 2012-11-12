50ff19006b1048882cacc6f1d6b8154cfcd46872
Assaf Gordon <assafgordon@gmail.com>
2012-11-12 18:44:50 -0500

    Emscripten Pre/Post scripts: fix for -O2 + closure
    Also see:
    https://github.com/kripken/emscripten/issues/704

--- js_emsc/pre_AWK.js
+++ js_emsc/pre_AWK.js
@@ -6,25 +6,28 @@
 run_web_awk = function(program_source, input_text)
 {
 	var Module = {
-		noInitialRun: true,
+		"noInitialRun": true,
+
 		arguments: [ "-f", "/my_program.awk", "/my_input.txt"],
+
 		reset_outputs: function() {
 			Module.awk_stdout = "" ;
 			Module.awk_stderr = "" ;
 		},
-		preRun: function() {
+		"preRun": function() {
 			FS.createDataFile('/', 'my_program.awk', program_source, true, false);
 			FS.createDataFile('/', 'my_input.txt', input_text, true, false);
 		},
-		print: function(text) {
+		"print": function(text) {
 			Module.awk_stdout += text + "\n" ;
 			//console.log("current stdout = " + Module.awk_stdout);
 		},
-		printErr : function(text) {
+		"printErr" : function(text) {
 			console.log(text);
 			Module.awk_stderr += text + "\n";
 		},
 	};
+	//Module["printErr"] = Module["foo_printErr"];
 
 	Module.reset_outputs();
 
